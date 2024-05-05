1525e1f4dc4bc56c98adf07e2d2b72ebc9d6aa3b
“tsukasatakahashi21” <tukaburu13@gmail.com>
2024-05-05 18:20:30 +0900

    feat: トップページの作成

--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,23 @@
-<?php
-echo 'Welcome TECH QUEST!';
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>sample</title>
+    </head>
+    <body>
+        <form action="complete.php" method="post">
+            <br />
+            <input type="text" name="title" size="50" value="" placeholder="回答者名"/><br />
+            回答者名
+            <br />
+            <input type="text" name="email" size="50" value="" placeholder="好きな食べ物"/><br />
+            好きな食べ物をご記入ください
+            <br />
+            <textarea name="content" cols="50" rows="5" placeholder="趣味"></textarea><br />
+            趣味をご記入してください
+            <br />
+ 
+            <input type="submit" value="送信" />
+        </form>
+    </body>
+</html>
