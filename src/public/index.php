2f18e803019eee3fd25c9475cfa7f5b1a5f1b4e8
“tsukasatakahashi21” <tukaburu13@gmail.com>
2024-05-05 18:38:45 +0900

    fix: トップページの修正

--- src/public/index.php
+++ src/public/index.php
@@ -6,17 +6,13 @@
     </head>
     <body>
         <form action="complete.php" method="post">
-            <br />
-            <input type="text" name="title" size="50" value="" placeholder="回答者名"/><br />
-            回答者名
-            <br />
-            <input type="text" name="email" size="50" value="" placeholder="好きな食べ物"/><br />
-            好きな食べ物をご記入ください
-            <br />
-            <textarea name="content" cols="50" rows="5" placeholder="趣味"></textarea><br />
-            趣味をご記入してください
-            <br />
- 
+            <p>回答者名</p>
+            <input type="text" name="title" size="50" value="" placeholder="回答者名"/>
+            <p>好きな食べ物をご記入ください</p>
+            <input type="text" name="email" size="50" value="" placeholder="好きな食べ物"/>
+            <p>趣味をご記入してください</p>
+            <textarea name="content" cols="50" rows="5" placeholder="趣味"></textarea>
+            <br>
             <input type="submit" value="送信" />
         </form>
     </body>
